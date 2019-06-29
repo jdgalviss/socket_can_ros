41f7722c977634a362406e9825c329528c2bb1fc
Juanda Galvis <juangalvis@kiwicampus.com>
2019-06-28 21:22:18 -0500

    feat: receiving status msgs

--- include/socket_can_ros/CANRos.h
+++ include/socket_can_ros/CANRos.h
@@ -12,11 +12,16 @@
 #include <geometry_msgs/TwistStamped.h>
 #include <std_msgs/String.h>
 #include <std_msgs/Float32.h>
+#include <std_msgs/Bool.h>
 #include <mavros_msgs/State.h>
 #include <mavros_msgs/CommandBool.h>
-#include <pwm/PwmOut.h>
+#include <socket_can_ros/PwmOut.h>
+#include <socket_can_ros/Configuration.h>
+#include <socket_can_ros/Motors.h>
+#include <socket_can_ros/TestMotors.h>
 #include <utils/console.h>
 #include <utils/debugger.h>
+#include <sensor_msgs/BatteryState.h>
 #include <vector>
 
 #define CONFIGURATION_CMD_ID 0x01
@@ -30,6 +35,19 @@
 #define DEBUG_CMD_ID 0x09
 #define STATUS_CMD_ID 0x0A
 
+#define WHEELS_BAUDRATE_DEFAULT 0x14
+#define BATTERY_STATUS_BAUDRATE_DEFAULT 0x14
+#define WHEEL_CONTROL_MODE_DEFAULT 0x00
+#define OPERATION_MODE_DEFAULT 0x00
+#define NUMBER_BATTERY_CELLS_DEFAULT 0x04
+#define MOTORS_MODEL_DEFAULT 0x00
+
+#define RIGHT_FRONT_WHEEL 0x01
+#define RIGHT_REAR_WHEEL 0x02
+#define LEFT_FRONT_WHEEL 0x04
+#define LEFT_REAR_WHEEL 0x08
+
+
 class CANRos{
     public:
         CANRos(ros::NodeHandle *nh);
@@ -42,28 +60,43 @@ class CANRos{
         std::vector<float> controls_;
         std::vector<float> offsets_;
         std::vector<float> inverts_;
-
-
-        mavros_msgs::State motor_drive_status_;
-        std_msgs::String can_driver_status_msg_;
-        pwm::PwmOut pwm_out_msg_;
         std::vector<uint16_t> raw_pwm_out_;
 
+        // Published msgs
+        mavros_msgs::State motor_drive_status_;
+        std_msgs::String can_driver_status_msg_;
+        socket_can_ros::PwmOut pwm_out_msg_;
+        socket_can_ros::Motors motors_status_;
+        socket_can_ros::TestMotors test_motors_response_;
+        sensor_msgs::BatteryState battery_msg_;
+
+
+        // Subscribed to msgs
+        socket_can_ros::Configuration chassis_configuration_;
 
         // Publishers
         ros::Publisher motor_drive_status_pub_;
         ros::Publisher pwm_out_pub_;
         ros::Publisher pwm_status_pub_;
+        ros::Publisher motor_status_pub_;
+        ros::Publisher test_motors_pub_;
+        ros::Publisher battery_pub_; 
 
         //Subscribers
         ros::Subscriber actuator_control_sub_;
+        ros::Subscriber chassis_config_sub_;
+        ros::Subscriber motors_sleep_sub_;
 
         // Services
         ros::ServiceServer arming_service_;
 
+        // Timers
+        ros::Timer error_timer_;
+
         Debugger *dbg_;
         CANDriver *can_driver_;
         float wheel_max_rpm_;
+        float motor_max_current_;
         int debug_level_;
         const char *interface_name_ = "vcan0";
         float radius_;
@@ -71,10 +104,14 @@ class CANRos{
 
         // Member functions
         void ActuatorControlCb(const geometry_msgs::TwistStamped::ConstPtr &msg);
+        void ChassisConfigCb(const socket_can_ros::Configuration::ConstPtr &msg);
+        void MotorsSleepCb(const std_msgs::Bool::ConstPtr &msg);
+        void ErrorTimerCb(const ros::TimerEvent &event);
+
         uint8_t RadsToDigital(float control);
         bool Arm(mavros_msgs::CommandBool::Request &req, mavros_msgs::CommandBool::Response &res);
         void SendMotorsCmd();
-
-
+        void SendChassisConfiguration();
+        void PublishChassisStatus(struct can_frame *frame);
 };
 #endif
--- src/can_ros.cpp
+++ src/can_ros.cpp
@@ -10,19 +10,6 @@
 CANRos::CANRos(ros::NodeHandle *nh)
 {
     n_ = nh;
-    // ============ Publishers ===========
-    // Motors control
-    motor_drive_status_pub_ = n_->advertise<mavros_msgs::State>("/kiwibot/driveController/status", 10);
-    pwm_out_pub_ = n_->advertise<pwm::PwmOut>("/kiwibot/pwm/out", 10);
-    pwm_status_pub_ = n_->advertise<std_msgs::String>("/kiwibot/pwm/status", 10);
-
-    // ============ Subscribers ===========
-    // Motors control
-    actuator_control_sub_ = n_->subscribe<geometry_msgs::TwistStamped>("/kiwibot/actuator_control", 1000, &CANRos::ActuatorControlCb, this);
-
-    // ============ Services ===========
-    // Motors control
-    arming_service_ = n_->advertiseService("/mavros/cmd/arming", &CANRos::Arm, this);
 
     //Initialize Debug level
     dbg_ = new Debugger;
@@ -41,6 +28,24 @@ CANRos::CANRos(ros::NodeHandle *nh)
     try
     {
         can_driver_ = new CANDriver(interface_name_);
+        // ============ Publishers ===========
+        // Motors control
+        motor_drive_status_pub_ = n_->advertise<mavros_msgs::State>("/kiwibot/driveController/status", 10);
+        pwm_out_pub_ = n_->advertise<socket_can_ros::PwmOut>("/kiwibot/pwm/out", 10);
+        pwm_status_pub_ = n_->advertise<std_msgs::String>("/kiwibot/pwm/status", 10);
+        motor_status_pub_ = n_->advertise<socket_can_ros::Motors>("/kiwibot/motor/status", 10);
+        test_motors_pub_ = n_->advertise<socket_can_ros::TestMotors>("/kiwibot/test_motors/response", 10);
+        battery_pub_ = n_->advertise<sensor_msgs::BatteryState>("/kiwibot/sensors/battery_can", 10);
+
+        // ============ Subscribers ===========
+        // Motors control
+        actuator_control_sub_ = n_->subscribe<geometry_msgs::TwistStamped>("/kiwibot/actuator_control", 1000, &CANRos::ActuatorControlCb, this);
+        chassis_config_sub_ = n_->subscribe<socket_can_ros::Configuration>("/kiwibot/chassis_configuration", 1000, &CANRos::ChassisConfigCb, this);
+        motors_sleep_sub_ = n_->subscribe<std_msgs::Bool>("/kiwibot/sleep", 1000, &CANRos::MotorsSleepCb, this);
+
+        // ============ Services ===========
+        // Motors control
+        arming_service_ = n_->advertiseService("/mavros/cmd/arming", &CANRos::Arm, this);
     }
     catch (const std::system_error &error)
     {
@@ -56,13 +61,38 @@ CANRos::CANRos(ros::NodeHandle *nh)
     radius_ = getEnv("WHEEL_RADIUS", 0.074f);
     robot_length_ = getEnv("ROBOT_LENGTH", 0.4f);
     wheel_max_rpm_ = getEnv("WHEEL_MAX_RPM", 165.0f);
+    motor_max_current_ = getEnv("MOTOR_MAX_CURRENT", 165.0f);
+
     controls_ = {0.0f, 0.0f, 0.0f};
     offsets_ = {0.0f, 0.0f, 0.0f};
     inverts_ = {1.0f, 1.0f, 1.0f};
     raw_pwm_out_.resize(3);
     inverts_.at(0) = -1; //Invert the Steering control
 
+    //Initial configuration
+    chassis_configuration_.wheels_baudrate = WHEELS_BAUDRATE_DEFAULT;
+    chassis_configuration_.battery_status_baudrate = BATTERY_STATUS_BAUDRATE_DEFAULT;
+    chassis_configuration_.wheel_control_mode = WHEEL_CONTROL_MODE_DEFAULT;
+    chassis_configuration_.operation_mode = OPERATION_MODE_DEFAULT;
+    chassis_configuration_.number_battery_cells = NUMBER_BATTERY_CELLS_DEFAULT;
+    chassis_configuration_.motors_model = MOTORS_MODEL_DEFAULT;
+    SendChassisConfiguration();
 
+    // Initialize motor status
+    motors_status_.rpm = {0.0, 0.0, 0.0, 0.0};
+    motors_status_.current = {0.0, 0.0, 0.0, 0.0};
+    motors_status_.error_status = {0, 0, 0, 0};
+    test_motors_response_.status = {0, 0, 0, 0};
+
+    error_timer_ = n_->createTimer(ros::Duration(0.1), &CANRos::ErrorTimerCb, this);
+    error_timer_.stop();
+}
+
+void CANRos::ErrorTimerCb(const ros::TimerEvent &event)
+{
+    error_timer_.stop();
+    for (int i = 0; i < 4; ++i)
+        motors_status_.error_status[i] = 0;
 }
 
 void CANRos::ActuatorControlCb(const geometry_msgs::TwistStamped::ConstPtr &msg)
@@ -70,6 +100,37 @@ void CANRos::ActuatorControlCb(const geometry_msgs::TwistStamped::ConstPtr &msg)
     controls_.at(0) = msg->twist.angular.z; //steering
     controls_.at(1) = msg->twist.linear.x;  //throttle
     SendMotorsCmd();
+}
+
+void CANRos::ChassisConfigCb(const socket_can_ros::Configuration::ConstPtr &msg)
+{
+    chassis_configuration_ = *msg;
+    SendChassisConfiguration();
+    ROS_INFO("-----------------Chassis Configured!---------------");
+}
+
+void CANRos::MotorsSleepCb(const std_msgs::Bool::ConstPtr &msg)
+{
+    if (msg->data)
+    {
+        uint8_t data[1] = {0x02};
+        can_driver_->CANWrite(ARM_CMD_ID, 1, data);
+    }
+    ROS_INFO("-----------------Motors Sleep cmd-----------------");
+}
+
+bool CANRos::Arm(mavros_msgs::CommandBool::Request &req, mavros_msgs::CommandBool::Response &res)
+{
+    uint8_t data[1];
+    data[0] = 0x00;
+    if (req.value && !motor_drive_status_.armed)
+        data[0] = 0x01;
+    if (!req.value && motor_drive_status_.armed)
+        data[0] = 0x00;
+    can_driver_->CANWrite(ARM_CMD_ID, 1, data);
+    res.success = true;
+    res.result = 0;
+    return true;
 }
 
 uint8_t CANRos::RadsToDigital(float control)
@@ -81,23 +142,76 @@ uint8_t CANRos::RadsToDigital(float control)
     return (uint8_t)std::abs(digital_value);
 }
 
-bool CANRos::Arm(mavros_msgs::CommandBool::Request &req, mavros_msgs::CommandBool::Response &res)
+void CANRos::PublishChassisStatus(struct can_frame *frame)
 {
-    if (req.value && !motor_drive_status_.armed)
-    { //arm pixhawk
-        motor_drive_status_.armed = true;
-        motor_drive_status_pub_.publish(motor_drive_status_);
-        dbg_->print(dbg_->DEBUG_LEVEL_1, "Arming Jethawk");
-    }
-    if (!req.value && motor_drive_status_.armed)
+    switch (frame->can_id)
     {
-        motor_drive_status_.armed = false;
-        motor_drive_status_pub_.publish(motor_drive_status_);
-        dbg_->print(dbg_->DEBUG_LEVEL_1, "Disrming Jethawk");
+    case WHEELS_INFO_ID:
+    {
+        uint8_t dir_b = frame->data[0];
+        bool directions_bool[4] = {((dir_b & RIGHT_FRONT_WHEEL) == 0x00), ((dir_b & RIGHT_REAR_WHEEL) == 0x00), ((dir_b & LEFT_FRONT_WHEEL) == 0x00), ((dir_b & LEFT_REAR_WHEEL) == 0x00)};
+        int directions[4];
+        for (int i = 0; i < 4; ++i)
+            directions[i] = directions_bool[i] ? 1 : -1;
+        for (int i = 0; i < 4; ++i)
+            motors_status_.rpm[i] = ((float)directions[i] * (float)frame->data[i + 1]) * wheel_max_rpm_ / 255.0f;
+        motors_status_.header.stamp = ros::Time::now();
+        motor_status_pub_.publish(motors_status_);
+        break;
     }
-    res.success = true;
-    res.result = 0;
-    return true;
+    case MOTORS_CURRENT_ID:
+    {
+        for (int i = 0; i < frame->can_dlc; ++i)
+            motors_status_.current[i] = (float)frame->data[i] * motor_max_current_ / 255.0;
+        break;
+    }
+    case ERROR_CMD_ID:
+    {
+        error_timer_.setPeriod(ros::Duration(1.5), true); // IMPORTANT: Check error msg logic (implement timer?)
+        error_timer_.start();
+        for (int i = 0; i < frame->can_dlc; ++i)
+            motors_status_.error_status[i] = frame->data[i];
+        break;
+    }
+    break;
+    case TEST_REPORT_CMD_ID:
+    {
+        for (int i = 0; i < frame->can_dlc; ++i)
+            test_motors_response_.status[i] = frame->data[i];
+        test_motors_pub_.publish(test_motors_response_);
+        break;
+    }
+
+    case STATUS_CMD_ID:
+    {
+        if (frame->data[0] = 0x01)
+        {
+            motor_drive_status_.armed = true;
+            motor_drive_status_pub_.publish(motor_drive_status_);
+            dbg_->print(dbg_->DEBUG_LEVEL_1, "Arming Jethawk");
+        }
+        else
+        {
+            motor_drive_status_.armed = false;
+            motor_drive_status_pub_.publish(motor_drive_status_);
+            dbg_->print(dbg_->DEBUG_LEVEL_1, "Disarming Jethawk");
+        }
+        // battery
+        battery_msg_.header.stamp = ros::Time::now();
+        battery_msg_.voltage = (float)frame->data[1] + (float)frame->data[2]/255.0; //IMPORTANT: CHECK SCALE
+        break;
+    }
+    default:
+        ROS_INFO("si");
+        break;
+    }
+}
+
+void CANRos::SendChassisConfiguration()
+{
+    uint8_t data[6] = {chassis_configuration_.wheels_baudrate, chassis_configuration_.battery_status_baudrate, chassis_configuration_.wheel_control_mode,
+                       chassis_configuration_.operation_mode, chassis_configuration_.number_battery_cells, chassis_configuration_.motors_model};
+    can_driver_->CANWrite(CONFIGURATION_CMD_ID, 6, data);
 }
 
 void CANRos::SendMotorsCmd()
@@ -113,11 +227,12 @@ void CANRos::SendMotorsCmd()
     float w_right = -(2.0f * controls_.at(1) - controls_.at(0) * robot_length_) / (2.0f * radius_);
     uint8_t w_left_dig = RadsToDigital(w_left);
     uint8_t w_right_dig = RadsToDigital(w_right);
-    uint8_t directions = 0xAA; //brake on all motors
+    uint8_t directions = 0xAA;
+    // brake on all motors
     if (w_left > 0)
     {
         if (w_right < 0)
-            directions = 0x50; //left CCW - right CW (go forward)
+            directions = 0x05; //left CCW - right CW (go forward)
         else
             directions = 0x55; //right CCW - left CCW (turn CCW)
     }
@@ -126,11 +241,13 @@ void CANRos::SendMotorsCmd()
         if (w_right < 0)
             directions = 0x00; //right CW - left CW (turn CW)
         else
-            directions = 0x05; //right CCW - left CW (go backward)
+            directions = 0x50; //right CCW - left CW (go backward)
     }
     uint8_t data[5] = {directions, w_left_dig, w_left_dig, w_right_dig, w_right_dig};
-    can_driver_->CANWrite(MOTORS_CONTROL_RAW_ID, 5, data);
-
+    if (chassis_configuration_.wheel_control_mode == 0x00)
+        can_driver_->CANWrite(WHEELS_CONTROL_RPM_ID, 5, data);
+    else if (chassis_configuration_.wheel_control_mode == 0x01)
+        can_driver_->CANWrite(MOTORS_CONTROL_RAW_ID, 5, data);
     // Calculate speeds on wheels
     pwm_out_msg_.channels = raw_pwm_out_;
     pwm_out_pub_.publish(pwm_out_msg_);
@@ -138,13 +255,16 @@ void CANRos::SendMotorsCmd()
     pwm_status_pub_.publish(can_driver_status_msg_);
 }
 
-void CANRos::ReadCANBus(){
-    struct can_frame *frame_;
-    frame_ = can_driver_->ReadMsg();
-    if(frame_){
-        for(int i=0; i<frame_->can_dlc; ++i){
-            ROS_INFO("index: %i - value: %i \n", i, frame_->data[i]);
+void CANRos::ReadCANBus()
+{
+    struct can_frame *frame;
+    frame = can_driver_->ReadMsg();
+    if (frame)
+    {
+        PublishChassisStatus(frame);
+        for (int i = 0; i < frame->can_dlc; ++i)
+        {
+            ROS_INFO("index: %i - value: %i \n", i, frame->data[i]);
         }
     }
 }
-
